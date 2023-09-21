898eb9a33bea6abb2da3a9bb2af710cf4aefd92f
yaroslav-zoryanyy <nemobroun@gmail.com>
2023-09-21 20:25:41 +0300

    mobile menu start

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,12 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
 */
+
+//Мобільне меню
+let Menu = document.querySelectorAll(".burger_menu");
+const List = document.querySelector(".header_list");
+Menu.forEach(function(item){
+    item.addEventListener("click",function(){
+        List.classList.toggle('active');
+    });
+});
