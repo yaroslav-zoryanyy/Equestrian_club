fccdf4ae10b4abd59356bf16facd7799ba914ec1
yaroslav-zoryanyy <nemobroun@gmail.com>
2023-09-25 22:35:23 +0300

    gallery swiper start

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,19 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-/*
 import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
+const swiper = new Swiper('.swiper',{
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
 
 //Мобільне меню
 let Menu = document.querySelectorAll(".burger_menu");
