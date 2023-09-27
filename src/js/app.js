847c77ac00629966b9fd24f390c16ef9dc3b3d3d
yaroslav-zoryanyy <nemobroun@gmail.com>
2023-09-27 15:49:49 +0300

    swiper demo

--- src/js/app.js
+++ src/js/app.js
@@ -2,19 +2,21 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper('.swiper',{
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "progressbar",
-      clickable: true
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    }
-  });
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// const swiper = new Swiper('.swiper',{
+//     grabCursor: true,
+//     slidesPerView: 1,
+//     slidesPerView: "auto",
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       type: "progressbar",
+//       clickable: true,
+//     },
+//   });
 
 //Мобільне меню
 let Menu = document.querySelectorAll(".burger_menu");
